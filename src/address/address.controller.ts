02c0bba0e3853a4532e13cc4371deef9c02a8b08
Guilherme Daher <guilherme.daher@venhapranuvem.com.br>
2023-04-20 12:14:19 -0300

    criação do decorator de userId

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -11,17 +11,18 @@ import { AddressService } from './address.service';
 import AddressEntity from './entities/address.entity';
 import { UserType } from 'src/user/enum/userType.enum';
 import { Roles } from 'src/decorators/roles.decorator';
+import { UserId } from 'src/decorators/userId.decorator';
 
+@Roles( UserType.User)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
-  @Roles( UserType.User)
-  @Post('/:userId')
+  @Post()
   @UsePipes(ValidationPipe)
   async createAddress(
     @Body() createAddressDto: CreateAddressDto,
-    @Param('userId') user_id: number,
+    @UserId() user_id: number,
   ): Promise<AddressEntity> {
     return this.addressService.createAddress(createAddressDto, user_id);
   }
---
+++ src/utils/base-64-converter.ts
@@ -0,0 +1,11 @@
+import LoginPayload from "src/auth/dto/loginPayload.dto"
+
+export const authorizationToLoginPayload = (authorization: string): LoginPayload | undefined => {
+    const authorizationSplitted = authorization.split('.');
+
+    if(authorization.length < 3 || !authorizationSplitted[1]){
+        return undefined;
+    } 
+
+    return JSON.parse(Buffer.from(authorizationSplitted[1], 'base64').toString('ascii'))
+}
