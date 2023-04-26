86637d724ea954f505ad34e6fe3ba814bfcbfa38
Guilherme Daher <guilherme.daher@venhapranuvem.com.br>
2023-04-25 21:34:21 -0300

    criação testes unitários de cache

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,9 +1,4 @@
-import {
-  CACHE_MANAGER,
-  Inject,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import CityEntity from './entities/city.entity';
 import { Repository } from 'typeorm';
